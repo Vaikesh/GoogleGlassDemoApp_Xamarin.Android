9006a170759bba712e9855fa693aa4df18d897ae
Vaikesh K P <vaikesh4leo@gmail.com>
2015-10-12 16:10:28 +0530

    Google Glass Demo App by Xamarin.Android
    Here is a demo app of Google Glass created by me using Xamarin.Android.
    This is a great stepping stone in my career to program in Google Glass.

---
+++ GlassDemoApp/Activity1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Com.Google.Android.Glass.App;
+
+namespace GlassDemoApp
+{
+    [Activity(Label = "GlassDemoApp", MainLauncher = true, Icon = "@drawable/icon")]
+    [IntentFilter(new[] { "com.google.android.glass.action.VOICE_TRIGGER" })]
+    [MetaData("com.google.android.glass.VoiceTrigger", Resource = "@xml/use_csharp")]
+    public class Activity1 : Activity
+    {
+   
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            var card = new Card(this);
+            card.SetText("Xamarin on Glass is awesome");
+
+            SetContentView(card.ToView());
+
+        }
+    }
+}
+
